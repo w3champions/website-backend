ba2733260aa713f14ecb3dd069d1f6db777beeb9
Simon Heiss <simonheiss87@gmail.com>
2020-03-27 21:56:03 +0100

    first commit

---
+++ W3ChampionsStatistikSyncer/Controllers/MatchEventsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace W3ChampionsStatistikSyncer.Controllers
+{
+    [ApiController]
+    [Route("api/matchevents")]
+    public class MatchEventsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PushEvents(IEnumerable<dynamic> events)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ W3ChampionsStatistikSyncer/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace W3ChampionsStatistikSyncer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
