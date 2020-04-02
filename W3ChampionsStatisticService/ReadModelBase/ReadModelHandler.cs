b77052152b0d35d7b4f12dd6567770d4a56406b6
Simon Heiss <simonheiss87@gmail.com>
2020-04-02 09:20:58 +0200

    rm handler now has better logging

--- W3ChampionsStatisticService/ReadModelBase/ReadModelHandler.cs
+++ W3ChampionsStatisticService/ReadModelBase/ReadModelHandler.cs
@@ -1,5 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using W3ChampionsStatisticService.Ports;
 
 namespace W3ChampionsStatisticService.ReadModelBase
@@ -9,15 +12,18 @@ namespace W3ChampionsStatisticService.ReadModelBase
         private readonly IMatchEventRepository _eventRepository;
         private readonly IVersionRepository _versionRepository;
         private readonly T _innerHandler;
+        private readonly ILogger<ReadModelHandler<T>> _logger;
 
         public ReadModelHandler(
             IMatchEventRepository eventRepository,
             IVersionRepository versionRepository,
-            T innerHandler)
+            T innerHandler,
+            ILogger<ReadModelHandler<T>> logger = null)
         {
             _eventRepository = eventRepository;
             _versionRepository = versionRepository;
             _innerHandler = innerHandler;
+            _logger = logger ?? new Logger<ReadModelHandler<T>>(new NullLoggerFactory());
         }
 
         public async Task Update()
@@ -29,8 +35,16 @@ namespace W3ChampionsStatisticService.ReadModelBase
             {
                 foreach (var nextEvent in nextEvents)
                 {
-                    await _innerHandler.Update(nextEvent);
-                    await _versionRepository.SaveLastVersion<T>(nextEvent.Id.ToString());
+                    try
+                    {
+                        await _innerHandler.Update(nextEvent);
+                        await _versionRepository.SaveLastVersion<T>(nextEvent.Id.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, $"ReadmodelHandler: {typeof(T).Name} died on event{nextEvent.Id}");
+                        throw;
+                    }
                 }
 
                 nextEvents = await _eventRepository.Load(nextEvents.Last().Id.ToString());
