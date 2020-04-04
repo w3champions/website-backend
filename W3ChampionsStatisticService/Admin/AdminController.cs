fef48ee11475dc3888b248a925437e282cf76387
Simon Heiss <simonheiss87@gmail.com>
2020-04-04 13:30:27 +0200

    added reset all route

--- W3ChampionsStatisticService/Admin/AdminController.cs
+++ W3ChampionsStatisticService/Admin/AdminController.cs
@@ -1,28 +1,37 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using W3ChampionsStatisticService.Ports;
+using MongoDB.Driver;
+using W3ChampionsStatisticService.MatchEvents;
 
 namespace W3ChampionsStatisticService.Admin
 {
     [ApiController]
-    [Route("api/ladder")]
+    [Route("api/admin")]
     public class AdminController : ControllerBase
     {
-        private readonly IPlayerRepository _playerRepository;
-        private readonly IVersionRepository _versionRepository;
+        private readonly DbConnctionInfo _connectionInfo;
+        private readonly string _databaseName = "W3Champions-Statistic-Service";
 
-        public AdminController(
-            IPlayerRepository playerRepository,
-            IVersionRepository versionRepository)
+        public AdminController(DbConnctionInfo connectionInfo)
         {
-            _playerRepository = playerRepository;
-            _versionRepository = versionRepository;
+            _connectionInfo = connectionInfo;
         }
 
         [HttpPut("resetAll")]
         public async Task<IActionResult> GetLadder(string authorization)
         {
             if (authorization != "ABD123F1-4AF5-4C55-B8D6-DCF7B5595991") return Unauthorized("Sorry H4ckerb0i");
+            var client = new MongoClient(_connectionInfo.ConnectionString);
+            var database = client.GetDatabase(_databaseName);
+            var listCollections = (await database.ListCollections().ToListAsync());
+            var collectionNames = listCollections.Select(c => c.RawValues.First().ToString());
+            var allCollectionsExceptEvents = collectionNames.Where(c => c != nameof(MatchFinishedEvent));
+
+            foreach (var collection in allCollectionsExceptEvents)
+            {
+                await database.DropCollectionAsync(collection);
+            }
 
             return Ok();
         }
