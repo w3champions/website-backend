e868914fccdd26518482f3aebaea6e8a99e5c358
Simon Heiss <simonheiss87@gmail.com>
2020-04-06 20:03:41 +0200

    match repo can now filter

--- W3ChampionsStatisticService/Matches/MatchRepository.cs
+++ W3ChampionsStatisticService/Matches/MatchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using W3ChampionsStatisticService.Ports;
@@ -17,8 +18,31 @@ namespace W3ChampionsStatisticService.Matches
             return Upsert(matchup, m => m.Id == matchup.Id);
         }
 
+        public async Task<List<Matchup>> LoadFor(string playerId, int gateWay = 10, int pageSize = 100, int offset = 0)
+        {
+            var database = CreateClient();
+
+            var mongoCollection = database.GetCollection<Matchup>(nameof(Matchup));
+
+            var strings = playerId.Split("#");
+            if (strings.Length != 2) return new List<Matchup>();
+            var name = strings[0];
+            var battleTag = strings[1];
+
+            var events = await mongoCollection
+                .Find(m => m.GateWay == gateWay && m.Teams
+                               .Any(t => t.Players
+                                   .Any(p => p.Name == name && p.BattleTag == battleTag)))
+                .SortBy(s => s.StartTime)
+                .Skip(offset)
+                .Limit(pageSize)
+                .ToListAsync();
+
+            return events;
+        }
+
         public async Task<List<Matchup>> Load(
-            int offset = default,
+            int offset = 0,
             int pageSize = 100,
             int gateWay = 10)
         {
--- WC3ChampionsStatisticService.UnitTests/MatchupRepoTests.cs
+++ WC3ChampionsStatisticService.UnitTests/MatchupRepoTests.cs
@@ -23,6 +23,40 @@ namespace WC3ChampionsStatisticService.UnitTests
         }
 
         [Test]
+        public async Task LoadAndSearch()
+        {
+            var matchRepository = new MatchRepository(DbConnctionInfo);
+
+            var matchFinishedEvent1 = TestDtoHelper.CreateFakeEvent();
+            var matchFinishedEvent2 = TestDtoHelper.CreateFakeEvent();
+            matchFinishedEvent1.match.players[1].battleTag = "Peter#123";
+
+            await matchRepository.Insert(new Matchup(matchFinishedEvent1));
+            await matchRepository.Insert(new Matchup(matchFinishedEvent2));
+
+            var matches = await matchRepository.LoadFor("Peter#123");
+
+            Assert.AreEqual(1, matches.Count);
+        }
+
+        [Test]
+        public async Task LoadAndSearch_InvalidString()
+        {
+            var matchRepository = new MatchRepository(DbConnctionInfo);
+
+            var matchFinishedEvent1 = TestDtoHelper.CreateFakeEvent();
+            var matchFinishedEvent2 = TestDtoHelper.CreateFakeEvent();
+            matchFinishedEvent1.match.players[1].battleTag = "Peter#123";
+
+            await matchRepository.Insert(new Matchup(matchFinishedEvent1));
+            await matchRepository.Insert(new Matchup(matchFinishedEvent2));
+
+            var matches = await matchRepository.LoadFor("Peter123");
+
+            Assert.AreEqual(0, matches.Count);
+        }
+
+        [Test]
         public async Task Upsert()
         {
             var matchRepository = new MatchRepository(DbConnctionInfo);
