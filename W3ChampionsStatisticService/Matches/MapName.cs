1d831d21d9ae61ad31a22f528a6836f739895b90
Simon Heiss <simonheiss87@gmail.com>
2020-04-07 12:10:27 +0200

    mapname different

--- W3ChampionsStatisticService/Matches/MapName.cs
+++ W3ChampionsStatisticService/Matches/MapName.cs
@@ -6,7 +6,7 @@ namespace W3ChampionsStatisticService.Matches
     {
         public MapName(string matchMap)
         {
-            Name = matchMap.Split("/").Last().Replace(".w3x", "").Substring(3);
+            Name = matchMap.Split("/").Last().Replace(".w3x", "").Replace("_lv", "").Substring(3);
         }
 
         public string Name { get; }
