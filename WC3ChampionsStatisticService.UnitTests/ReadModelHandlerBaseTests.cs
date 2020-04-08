145811cd5f63f2aa2f1ecb90c5b8fbc95ace3f62
Simon Heiss <simonheiss87@gmail.com>
2020-04-09 00:46:21 +0200

    fixed tests

--- WC3ChampionsStatisticService.UnitTests/ReadModelHandlerBaseTests.cs
+++ WC3ChampionsStatisticService.UnitTests/ReadModelHandlerBaseTests.cs
@@ -38,12 +38,13 @@ namespace WC3ChampionsStatisticService.UnitTests
             mockMatchRepo.Verify(m => m.Insert(It.Is<Matchup>(ma => ma.Map == "amazonia")), Times.Once);
         }
 
+        [Test]
         public async Task InsertMatchesFail1()
         {
             var fakeEvent = TestDtoHelper.CreateFakeEvent();
 
             fakeEvent.match.map = "Maps/frozenthrone/community/(2)amazonia.w3x";
-            fakeEvent.match.state = 1;
+            fakeEvent.match.state = 3;
             var mockEvents = new Mock<IMatchEventRepository>();
             mockEvents.SetupSequence(m => m.Load(It.IsAny<string>(), It.IsAny<int>()))
                 .ReturnsAsync(new List<MatchFinishedEvent>() { fakeEvent })
@@ -63,29 +64,5 @@ namespace WC3ChampionsStatisticService.UnitTests
             mockMatchRepo.Verify(m => m.Insert(It.IsAny<Matchup>()), Times.Never);
         }
 
-        public async Task InsertMatchesFail2()
-        {
-            var fakeEvent = TestDtoHelper.CreateFakeEvent();
-
-            fakeEvent.match.map = "Maps/frozenthrone/community/(2)amazonia.w3x";
-            fakeEvent.match.state = 0;
-            var mockEvents = new Mock<IMatchEventRepository>();
-            mockEvents.SetupSequence(m => m.Load(It.IsAny<string>(), It.IsAny<int>()))
-                .ReturnsAsync(new List<MatchFinishedEvent>() { fakeEvent })
-                .ReturnsAsync(new List<MatchFinishedEvent>());
-
-            var mockMatchRepo = new Mock<IMatchRepository>();
-
-            var versionRepository = new Mock<IVersionRepository>();
-
-            var handler = new ReadModelHandler<MatchReadModelHandler>(
-                mockEvents.Object,
-                versionRepository.Object,
-                new MatchReadModelHandler(mockMatchRepo.Object));
-
-            await handler.Update();
-
-            mockMatchRepo.Verify(m => m.Insert(It.IsAny<Matchup>()), Times.Never);
-        }
     }
 }
