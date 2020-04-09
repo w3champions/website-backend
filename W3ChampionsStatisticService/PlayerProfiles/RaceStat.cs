db9049cc18a2603f70c3b506a7a7413b6973b0b4
Simon Heiss <simonheiss87@gmail.com>
2020-04-09 22:50:36 +0200

    small refactoring of win losses

--- W3ChampionsStatisticService/PlayerProfiles/RaceStat.cs
+++ W3ChampionsStatisticService/PlayerProfiles/RaceStat.cs
@@ -2,7 +2,7 @@
 
 namespace W3ChampionsStatisticService.PlayerProfiles
 {
-    public class RaceStat
+    public class RaceStat : WinLoss
     {
         public RaceStat(Race race)
         {
@@ -10,20 +10,5 @@ namespace W3ChampionsStatisticService.PlayerProfiles
         }
 
         public Race Race { set; get; }
-        public int Wins { set; get; }
-        public int Losses { set; get; }
-        public double Winrate => new WinRate(Wins, Losses).Rate;
-
-        public void Update(bool won)
-        {
-            if (won)
-            {
-                Wins++;
-            }
-            else
-            {
-                Losses++;
-            }
-        }
     }
 }
