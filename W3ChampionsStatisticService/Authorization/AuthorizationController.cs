fb35a8a445269b01c8c1b9c26051ce8176f060e9
Simon Heiss <simonheiss87@gmail.com>
2020-04-15 21:11:02 +0200

    get token endpoint works

---
+++ W3ChampionsStatisticService/Authorization/AuthorizationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace W3ChampionsStatisticService.Authorization
+{
+
+    [ApiController]
+    [Route("api/oauth")]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet("token")]
+        public async Task<IActionResult> ResetAllReadModels([FromQuery] string code, [FromQuery] string redirectUri)
+        {
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://eu.battle.net/oauth/token");
+            var res = await httpClient.GetAsync(
+                $"?region=eu&code={code}&grant_type=authorization_code&redirect_uri={redirectUri}&client_id=d7bd6dd46e2842c8a680866759ad34c2&client_secret=7qs8iu2dcX4ZrURpIpezwZJHNM7OJmXg");
+            if (!res.IsSuccessStatusCode)
+            {
+                return Unauthorized("Sorry H4ckerb0i");
+            }
+
+            var readAsStringAsync = await res.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<BlizzardToken>(readAsStringAsync);
+            return Ok(token);
+        }
+    }
+}
