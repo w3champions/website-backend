e46740dfe9b24de86fa76cf2aca4f1bdfe8fc3ca
Simon Heiss <simonheiss87@gmail.com>
2020-05-03 16:32:57 +0200

    moved classes

---
+++ W3ChampionsStatisticService/Ladder/PlayerId.cs
@@ -0,0 +1,19 @@
+﻿namespace W3ChampionsStatisticService.Ladder
+{
+    public class PlayerId
+    {
+        public static PlayerId Create(string id, string nameTag)
+        {
+            return new PlayerId
+            {
+                Id = id,
+                Name = nameTag.Split("#")[0],
+                BattleTag = nameTag.Split("#")[1]
+            };
+        }
+
+        public string Name { get; set; }
+        public string BattleTag { get; set; }
+        public string Id { get; set; }
+    }
+}
--- W3ChampionsStatisticService/Ladder/PlayerOverview.cs
+++ W3ChampionsStatisticService/Ladder/PlayerOverview.cs
@@ -31,21 +31,4 @@ namespace W3ChampionsStatisticService.Ladder
             RecordWin(won);
         }
     }
-
-    public class PlayerId
-    {
-        public static PlayerId Create(string id, string nameTag)
-        {
-            return new PlayerId
-            {
-                Id = id,
-                Name = nameTag.Split("#")[0],
-                BattleTag = nameTag.Split("#")[1]
-            };
-        }
-
-        public string Name { get; set; }
-        public string BattleTag { get; set; }
-        public string Id { get; set; }
-    }
 }
