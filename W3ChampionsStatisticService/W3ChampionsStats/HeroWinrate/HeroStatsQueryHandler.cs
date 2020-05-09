8b91057053cbbd50a1a8b847c928882d7fdad508
Simon Heiss <simonheiss87@gmail.com>
2020-05-09 21:29:14 +0200

    not seems to work

--- W3ChampionsStatisticService/W3ChampionsStats/HeroWinrate/HeroStatsQueryHandler.cs
+++ W3ChampionsStatisticService/W3ChampionsStats/HeroWinrate/HeroStatsQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using W3ChampionsStatisticService.PlayerStats.RaceOnMapVersusRaceStats;
 using W3ChampionsStatisticService.Ports;
@@ -13,6 +15,7 @@ namespace W3ChampionsStatisticService.W3ChampionsStats.HeroWinrate
         {
             _w3StatsRepo = w3StatsRepo;
         }
+
         public async Task<WinLoss> GetStats(
             string first,
             string second,
@@ -27,18 +30,39 @@ namespace W3ChampionsStatisticService.W3ChampionsStats.HeroWinrate
                 if (second != "none") searchString += $"_{second}";
                 if (third != "none") searchString += $"_{third}";
                 var stats = await _w3StatsRepo.LoadHeroWinrate(searchString);
-                var heroWinrateDto = new HeroWinrateDto(new List<HeroWinRatePerHero> { stats }, opFirst, opSecond, opThird);
-                return heroWinrateDto.Winrate.WinLoss;
+                return HeroWinrateDto(new List<HeroWinRatePerHero> {stats}, opFirst, opSecond, opThird);
             }
             else
             {
                 if (second != "all") searchString += $"_{second}";
                 if (third != "all") searchString += $"_{third}";
                 var stats = await _w3StatsRepo.LoadHeroWinrateLike(searchString);
-                var heroWinrateDto = new HeroWinrateDto(stats, opFirst, opSecond, opThird);
-                return heroWinrateDto.Winrate.WinLoss;
+                return HeroWinrateDto(stats, opFirst, opSecond, opThird);
             }
+        }
 
+        private WinLoss HeroWinrateDto(List<HeroWinRatePerHero> stats, string opFirst, string opSecond, string opThird)
+        {
+            if (opSecond == "all") return CombineWinrates(stats, s => s.HeroCombo.StartsWith($"{opFirst}"));
+            if (opThird == "all") return CombineWinrates(stats, s => s.HeroCombo.StartsWith($"{opFirst}_{opSecond}"));
+            if (opSecond == "none") return CombineWinrates(stats, s => s.HeroCombo == $"{opFirst}");
+            if (opThird == "none") return CombineWinrates(stats, s => s.HeroCombo == $"{opFirst}_{opSecond}");
+
+            return CombineWinrates(stats, s => s.HeroCombo == $"{opFirst}_{opSecond}_{opThird}");
+        }
+
+        private WinLoss CombineWinrates(
+            List<HeroWinRatePerHero> stats,
+            Func<HeroWinRate, bool> func)
+        {
+            var winrates = stats.SelectMany(s => s.WinRates).Where(func).ToList();
+            var wins = winrates.Sum(w => w.WinLoss.Wins);
+            var losses = winrates.Sum(w => w.WinLoss.Losses);
+            return new WinLoss
+            {
+                Wins = wins,
+                Losses = losses
+            };
         }
     }
 }
