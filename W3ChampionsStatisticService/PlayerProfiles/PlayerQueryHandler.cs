0f4d4dc5861751ba40c589fc1c531f8fb062c543
Simon Heiss <simonheiss87@gmail.com>
2020-05-11 11:19:33 +0200

    go on sorting by at ranking

---
+++ W3ChampionsStatisticService/PlayerProfiles/PlayerQueryHandler.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using W3ChampionsStatisticService.Ladder;
+using W3ChampionsStatisticService.Matches;
+using W3ChampionsStatisticService.Ports;
+
+namespace W3ChampionsStatisticService.PlayerProfiles
+{
+    public class PlayerQueryHandler
+    {
+        private readonly IPlayerRepository _playerRepository;
+        private readonly IRankRepository _rankRepository;
+
+        public PlayerQueryHandler(
+            IPlayerRepository playerRepository,
+            IRankRepository rankRepository)
+        {
+            _playerRepository = playerRepository;
+            _rankRepository = rankRepository;
+        }
+        public async Task<PlayerProfile> Get(string battleTag)
+        {
+            var player = await _playerRepository.Load(battleTag) ?? PlayerProfile.Default();
+            var leaguesOfPlayer = await _rankRepository.LoadPlayerOfLeague(battleTag);
+            var allLeagues = await _rankRepository.LoadLeagueConstellation();
+
+            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_1v1, GateWay.Europe);
+            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_1v1, GateWay.Usa);
+            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_2v2_AT, GateWay.Europe);
+            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_2v2_AT, GateWay.Usa);
+            return player;
+        }
+
+         //way to shitty, do this with better rm one day
+        private void PopulateStats(
+            List<Rank> leaguesOfPlayer,
+            PlayerProfile player,
+            List<LeagueConstellation> allLeagues,
+            GameMode gameMode,
+            GateWay gateWay)
+        {
+            var gameModeIndex = gameMode switch
+            {
+                GameMode.GM_1v1 => 0,
+                GameMode.GM_2v2_AT => 1,
+                _ => 0
+            };
+
+            var searchedLeagues = leaguesOfPlayer.FirstOrDefault(l => l.GameMode == gameMode && l.Gateway == gateWay);
+            if (searchedLeagues != null)
+            {
+                player.GateWayStats
+                    .Single(g => g.GateWay == gateWay)
+                    .GameModeStats[gameModeIndex].Rank = searchedLeagues.RankNumber;
+                player.GateWayStats
+                    .Single(g => g.GateWay == gateWay)
+                    .GameModeStats[gameModeIndex].LeagueId = searchedLeagues.League;
+                player.GateWayStats
+                    .Single(g => g.GateWay == gateWay)
+                    .GameModeStats[gameModeIndex].LeagueOrder = allLeagues
+                    .Single(l => l.Gateway == gateWay && l.GameMode == gameMode)
+                    .Leagues
+                    .SingleOrDefault(l => l.Id == searchedLeagues.League)?.Order ?? 6;
+                player.GateWayStats
+                    .Single(g => g.GateWay == gateWay)
+                    .GameModeStats[gameModeIndex].Division = allLeagues
+                    .Single(l => l.Gateway == gateWay && l.GameMode == gameMode)
+                    .Leagues
+                    .SingleOrDefault(l => l.Id == searchedLeagues.League)?.Division ?? 0;
+            }
+        }
+    }
+}
--- W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
+++ W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
@@ -16,78 +16,23 @@ namespace W3ChampionsStatisticService.PlayerProfiles
     {
         private readonly IPlayerRepository _playerRepository;
         private readonly IRankRepository _rankRepository;
-        private readonly TrackingService _trackingService;
+        private readonly PlayerQueryHandler _playerQueryHandler;
 
         public PlayersController(
             IPlayerRepository playerRepository,
             IRankRepository rankRepository,
-            TrackingService trackingService)
+            PlayerQueryHandler playerQueryHandler)
         {
             _playerRepository = playerRepository;
             _rankRepository = rankRepository;
-            _trackingService = trackingService;
+            _playerQueryHandler = playerQueryHandler;
         }
 
         [HttpGet("{battleTag}")]
         public async Task<IActionResult> GetPlayer([FromRoute] string battleTag)
         {
-            var player = await _playerRepository.Load(battleTag) ?? PlayerProfile.Default();
-            var leaguesOfPlayer = await _rankRepository.LoadPlayerOfLeague(battleTag);
-            var allLeagues = await _rankRepository.LoadLeagueConstellation();
-
-            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_1v1, GateWay.Europe);
-            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_1v1, GateWay.Usa);
-            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_2v2_AT, GateWay.Europe);
-            PopulateStats(leaguesOfPlayer, player, allLeagues, GameMode.GM_2v2_AT, GateWay.Usa);
-
+            var player = await _playerQueryHandler.Get(battleTag);
             return Ok(player);
-        }
-
-        //way to shitty, do this with better rm one day
-        private void PopulateStats(
-            List<Rank> leaguesOfPlayer,
-            PlayerProfile player,
-            List<LeagueConstellation> allLeagues,
-            GameMode gameMode,
-            GateWay gateWay)
-        {
-            try
-            {
-                var gameModeIndex = gameMode switch
-                {
-                    GameMode.GM_1v1 => 0,
-                    GameMode.GM_2v2_AT => 1,
-                    _ => 0
-                };
-
-                var searchedLeagues = leaguesOfPlayer.FirstOrDefault(l => l.GameMode == gameMode && l.Gateway == gateWay);
-                if (searchedLeagues != null)
-                {
-                    player.GateWayStats
-                        .Single(g => g.GateWay == gateWay)
-                        .GameModeStats[gameModeIndex].Rank = searchedLeagues.RankNumber;
-                    player.GateWayStats
-                        .Single(g => g.GateWay == gateWay)
-                        .GameModeStats[gameModeIndex].LeagueId = searchedLeagues.League;
-                    player.GateWayStats
-                        .Single(g => g.GateWay == gateWay)
-                        .GameModeStats[gameModeIndex].LeagueOrder = allLeagues
-                        .Single(l => l.Gateway == gateWay && l.GameMode == gameMode)
-                        .Leagues
-                        .SingleOrDefault(l => l.Id == searchedLeagues.League)?.Order ?? 6;
-                    player.GateWayStats
-                        .Single(g => g.GateWay == gateWay)
-                        .GameModeStats[gameModeIndex].Division = allLeagues
-                        .Single(l => l.Gateway == gateWay && l.GameMode == gameMode)
-                        .Leagues
-                        .SingleOrDefault(l => l.Id == searchedLeagues.League)?.Division ?? 0;
-                }
-            }
-            catch (Exception e)
-            {
-                _trackingService.TrackException(e, $"could not find ladder for {player.BattleTag} gm {gameMode} gw {gateWay}");
-            }
-
         }
 
         [HttpGet("{battleTag}/winrate")]
