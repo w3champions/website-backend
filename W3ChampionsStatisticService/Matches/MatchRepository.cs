69035e6b7aeb52b927637c55b6c0e4e30b4691db
Simon Heiss <simonheiss87@gmail.com>
2020-05-13 16:10:51 +0200

    added gateway filter for profile search

--- W3ChampionsStatisticService/Matches/MatchRepository.cs
+++ W3ChampionsStatisticService/Matches/MatchRepository.cs
@@ -20,9 +20,9 @@ namespace W3ChampionsStatisticService.Matches
             return Upsert(matchup, m => m.Id == matchup.Id);
         }
 
-        public async Task<List<Matchup>> LoadFor(
-            string playerId,
+        public async Task<List<Matchup>> LoadFor(string playerId,
             string opponentId = null,
+            GateWay gateWay = GateWay.Undefined,
             GameMode gameMode = GameMode.Undefined,
             int pageSize = 100,
             int offset = 0)
@@ -35,6 +35,7 @@ namespace W3ChampionsStatisticService.Matches
             {
                 return await mongoCollection
                     .Find(m => (gameMode == GameMode.Undefined || m.GameMode == gameMode)
+                               && (gateWay == GateWay.Undefined || m.GateWay == gateWay)
                                && (m.Team1Players.Contains(playerId) || m.Team2Players.Contains(playerId)))
                     .SortByDescending(s => s.StartTime)
                     .Skip(offset)
@@ -43,7 +44,8 @@ namespace W3ChampionsStatisticService.Matches
             }
 
             return await mongoCollection
-                .Find(m =>  (gameMode == GameMode.Undefined || m.GameMode == gameMode) &&
+                .Find(m =>  (gameMode == GameMode.Undefined || m.GameMode == gameMode)
+                            && (gateWay == GateWay.Undefined || m.GateWay == gateWay) &&
                      ((m.Team1Players.Contains(playerId) && m.Team2Players.Contains(opponentId))
                     || (m.Team2Players.Contains(playerId) && m.Team1Players.Contains(opponentId))))
                 .SortByDescending(s => s.StartTime)
@@ -60,13 +62,15 @@ namespace W3ChampionsStatisticService.Matches
         public Task<long> CountFor(
             string playerId,
             string opponentId = null,
+            GateWay gateWay = GateWay.Undefined,
             GameMode gameMode = GameMode.Undefined)
         {
             var mongoCollection = CreateCollection<Matchup>();
             if (string.IsNullOrEmpty(opponentId))
             {
                 return mongoCollection.CountDocumentsAsync(m =>
-                    (gameMode == GameMode.Undefined || m.GameMode == gameMode) &&
+                    (gameMode == GameMode.Undefined || m.GameMode == gameMode)
+                    && (gateWay == GateWay.Undefined || m.GateWay == gateWay) &&
                     m.Teams
                         .Any(t => t.Players
                             .Any(p => p.BattleTag.Equals(playerId))));
