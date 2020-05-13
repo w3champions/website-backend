19e1750c1152d04fffa9d02bbcf78ab58d742a1f
Simon Heiss <simonheiss87@gmail.com>
2020-05-13 10:40:23 +0200

    race stats now sesoned and gatewayed

---
+++ W3ChampionsStatisticService/PlayerProfiles/RaceWinLossPerGateway.cs
@@ -0,0 +1,18 @@
+﻿using W3ChampionsStatisticService.CommonValueObjects;
+
+namespace W3ChampionsStatisticService.PlayerProfiles
+{
+    public class RaceWinLossPerGateway : WinLoss
+    {
+        public RaceWinLossPerGateway(Race race, GateWay gateWay, int season)
+        {
+            Race = race;
+            GateWay = gateWay;
+            Season = season;
+        }
+
+        public Race Race { get; set; }
+        public GateWay GateWay { get; set; }
+        public int Season { get; set; }
+    }
+}
