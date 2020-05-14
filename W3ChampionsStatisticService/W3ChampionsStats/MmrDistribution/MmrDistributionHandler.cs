728774b99fc57afcb8ebe0f0e0de8c18a0ecd01c
Simon Heiss <simonheiss87@gmail.com>
2020-05-14 22:56:38 +0200

    moved

---
+++ W3ChampionsStatisticService/W3ChampionsStats/MmrDistribution/MmrDistributionHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using W3ChampionsStatisticService.Ports;
+
+namespace W3ChampionsStatisticService.W3ChampionsStats.MmrDistribution
+{
+    public class MmrDistributionHandler
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public MmrDistributionHandler(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public async Task<MmrStats> GetDistributions(int season)
+        {
+            var mmrs = await _playerRepository.LoadMmrs(season);
+            var orderedMMrs = mmrs.OrderByDescending(m => m).ToList();
+            var ranges = Ranges(2325, 575, 25).ToList();
+            var highest = ranges.First();
+            var grouped = ranges.Select(r => new MmrCount(r, orderedMMrs.Count(x => Math.Abs(x - r) < 25 || x > highest))).ToList();
+            grouped.Remove(grouped.Last());
+            return new MmrStats(grouped, orderedMMrs);
+        }
+
+        private static IEnumerable<int> Ranges(int max, int min, int steps)
+        {
+            while (max > min)
+            {
+                max -= steps;
+                yield return max;
+            }
+        }
+    }
+
+    public class MmrStats
+    {
+
+        public int Top2PercentIndex { get; set; }
+        public int Top5PercentIndex { get; set; }
+        public int Top10PercentIndex { get; set; }
+        public int Top25ercentIndex { get; set; }
+        public int Top50PercentIndex { get; set; }
+
+        public List<MmrCount> DistributedMmrs { get; }
+
+        public MmrStats(List<MmrCount> distributedMmrs, List<int> mmrs)
+        {
+            DistributedMmrs = distributedMmrs;
+
+            Top2PercentIndex = DistributedMmrs.IndexOf(DistributedMmrs.Last(d => d.Mmr > mmrs[mmrs.Count / 50]));
+            Top5PercentIndex = DistributedMmrs.IndexOf(DistributedMmrs.Last(d => d.Mmr > mmrs[mmrs.Count / 20]));
+            Top10PercentIndex = DistributedMmrs.IndexOf(DistributedMmrs.Last(d => d.Mmr > mmrs[mmrs.Count / 10]));
+            Top25ercentIndex = DistributedMmrs.IndexOf(DistributedMmrs.Last(d => d.Mmr > mmrs[mmrs.Count / 4]));
+            Top50PercentIndex = DistributedMmrs.IndexOf(DistributedMmrs.Last(d => d.Mmr > mmrs[mmrs.Count / 2]));
+        }
+    }
+}
--- W3ChampionsStatisticService/W3ChampionsStats/W3CStatsController.cs
+++ W3ChampionsStatisticService/W3ChampionsStats/W3CStatsController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using W3ChampionsStatisticService.Ports;
 using W3ChampionsStatisticService.W3ChampionsStats.HeroWinrate;
+using W3ChampionsStatisticService.W3ChampionsStats.MmrDistribution;
 
 namespace W3ChampionsStatisticService.W3ChampionsStats
 {
@@ -13,15 +13,18 @@ namespace W3ChampionsStatisticService.W3ChampionsStats
     {
         private readonly IW3StatsRepo _w3StatsRepo;
         private readonly HeroStatsQueryHandler _heroStatsQueryHandler;
+        private readonly MmrDistributionHandler _mmrDistributionHandler;
         private readonly IPlayerRepository _playerRepository;
 
         public W3CStatsController(
             IW3StatsRepo w3StatsRepo,
             HeroStatsQueryHandler heroStatsQueryHandler,
+            MmrDistributionHandler mmrDistributionHandler,
             IPlayerRepository playerRepository)
         {
             _w3StatsRepo = w3StatsRepo;
             _heroStatsQueryHandler = heroStatsQueryHandler;
+            _mmrDistributionHandler = mmrDistributionHandler;
             _playerRepository = playerRepository;
         }
 
@@ -84,28 +87,11 @@ namespace W3ChampionsStatisticService.W3ChampionsStats
             return Ok(stats);
         }
 
-        public class MmrCount
-        {
-            public int Mmr { get; set; }
-            public int Count { get; set; }
-
-            public MmrCount(int mmr, int count)
-            {
-                Mmr = mmr;
-                Count = count;
-            }
-        }
-
         [HttpGet("mmr-distribution")]
         public async Task<IActionResult> GetMmrDistribution(int season)
         {
-            var mmrs = await _playerRepository.LoadMmrs(season);
-            var ranges = new[] { 2300, 2100, 1900, 1700, 1500, 1300, 1100 };
-            var highest = ranges.First();
-            var count = mmrs.Count(m => m < 1200 && m != 0);
-            var grouped = ranges.Select(r => new MmrCount(r, mmrs.Count(x => Math.Abs(x - r) < 100 || x > highest)));
-            grouped.Last().Count = count;
-            return Ok(grouped);
+            var mmrs = await _mmrDistributionHandler.GetDistributions(season);
+            return Ok(mmrs);
         }
     }
 }
