ecc119d561cecf1c298095dfa0873922fe0a6213
s.heiss <simonheiss87@gmail.com>
2020-06-02 00:32:00 +0200

    fix bson map (#33)
    * rename
    
    * small refactoring
    
    * revoke invite now possible
    
    * added a fix to clan invites
    
    * clanname in membership aswell
    
    * rejectin possible
    
    * leave clan possible
    
    * fix add member
    
    * fix now
    
    * removed sign function
    
    * added state machine for clan states
    
    * fix bson map

---
+++ W3ChampionsStatisticService/ReadModelBase/BsonExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson.Serialization;
+using W3ChampionsStatisticService.Clans.ClanStates;
+
+namespace W3ChampionsStatisticService.ReadModelBase
+{
+    public static class BsonExtensions
+    {
+        public static IServiceCollection AddSpecialBsonRegistrations(this IServiceCollection services)
+        {
+            BsonClassMap.RegisterClassMap<ClanState>(cm => {
+                cm.AutoMap();
+                cm.SetIsRootClass(true);
+
+                var featureType = typeof(ClanState);
+                featureType.Assembly.GetTypes()
+                    .Where(type => featureType.IsAssignableFrom(type)).ToList()
+                    .ForEach(type => cm.AddKnownType(type));
+            });
+
+            return services;
+        }
+    }
+}
