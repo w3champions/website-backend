4c22cac408b2eb22814e40aab487d4740445e850
s.heiss <simonheiss87@gmail.com>
2020-06-01 17:39:36 +0200

    Add clanname to membership (#28)
    * rename
    
    * small refactoring
    
    * revoke invite now possible
    
    * added a fix to clan invites
    
    * clanname in membership aswell

--- W3ChampionsStatisticService/Matches/MatchesController.cs
+++ W3ChampionsStatisticService/Matches/MatchesController.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using W3ChampionsStatisticService.CommonValueObjects;
