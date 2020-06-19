942b259eb88f6d7835fa9edf8a53b9f7d1a479dd
Simon Heiss <simonheiss87@gmail.com>
2020-06-19 12:16:53 +0200

    switched url

--- W3ChampionsStatisticService/Admin/AdminController.cs
+++ W3ChampionsStatisticService/Admin/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using W3ChampionsStatisticService.Ports;
 
 namespace W3ChampionsStatisticService.Admin
 {
@@ -7,10 +9,24 @@ namespace W3ChampionsStatisticService.Admin
     [Route("api/admin")]
     public class AdminController : ControllerBase
     {
+        private readonly IMatchRepository _matchRepository;
+
+        public AdminController(IMatchRepository matchRepository)
+        {
+            _matchRepository = matchRepository;
+        }
+
         [HttpGet("health-check")]
         public IActionResult HealthCheck()
         {
             return Ok();
+        }
+
+        [HttpGet("db-health-check")]
+        public async Task<IActionResult> DatabaseHealthCheck()
+        {
+            var countOnGoingMatches = await _matchRepository.CountOnGoingMatches();
+            return Ok(countOnGoingMatches);
         }
     }
 }
