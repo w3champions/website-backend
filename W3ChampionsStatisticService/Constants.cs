120e84a2803eab66b876971e29c6f83ad79927ab
Simon Heiss <simonheiss87@gmail.com>
2020-07-17 23:49:37 +0200

    test checkin

--- W3ChampionsStatisticService/Constants.cs
+++ W3ChampionsStatisticService/Constants.cs
@@ -4,15 +4,10 @@ namespace W3ChampionsStatisticService
 {
     public static class Constants
     {
-        public static List<string> ApprovedAdmins
-        {
-            get
-            {
-                return new List<string>{"gab#1546",
-                                "floss2xdaily#1987",
-                                "setcho#2271",
-                                "modmoto#2809"};
-            }
-        }
+        public static List<string> ApprovedAdmins =>
+            new List<string>{"gab#1546",
+                "floss2xdaily#1987",
+                "setcho#2271",
+                "modmoto#2809"};
     }
 }
