fe536724a6a2ab64bb38c6ea4c7357a427a8c0a3
Simon Heiss <simonheiss87@gmail.com>
2020-08-02 06:52:01 +0200

    added string mapping for maps and stats

--- W3ChampionsStatisticService/Matches/MapName.cs
+++ W3ChampionsStatisticService/Matches/MapName.cs
@@ -4,9 +4,51 @@ namespace W3ChampionsStatisticService.Matches
 {
     public class MapName
     {
-        public MapName(string matchMap)
+        public MapName(string map)
         {
-            Name = matchMap.Split("/").Last().Replace(".w3x", "").Replace("_lv", "").Substring(3);
+            var mapNampe = map.Split("/").Last().Replace(".w3x", "").Replace("_lv", "").Substring(3);
+            Name = ParseForLegacy(mapNampe);
+        }
+
+        private string ParseForLegacy(string mapName)
+        {
+            switch (mapName)
+            {
+                case "_1v1_autumnleaves_anon": return "_1v1_autumnleaves_anon";
+                case "_1v1_terenasstand_anon": return "terenasstand";
+                case "_1v1_lastrefuge_anon": return "lastrefuge";
+                case "_1v1_northernisles_anon": return "northernisles";
+                case "_1v1_amazonia_anon": return "amazonia";
+                case "_1v1_echoisles_anon": return "echoisles";
+                case "_1v1_concealedhill_anon": return "concealedhill";
+                case "_1v1_twistedmeadows_anon": return "twistedmeadows";
+
+                case "_gnollwood_anon": return "gnollwood";
+                case "_avalanche_anon": return "avalanche";
+                case "_battlegrounds_anon": return "battleground";
+                case "_cherryville_anon": return "cherryville";
+                case "_circleoffallenheroes_anon": return "circleoffallenheroes";
+                case "_deadlock_lv_anon": return "deadlock";
+                case "_feralas_lv_anon": return "feralas";
+                case "_fullscaleassault_anon": return "fullscaleassault";
+                case "_goldrush_anon": return "goldrush";
+                case "_goldshire_anon": return "goldshire";
+                case "_goleminthemist_lv_anon": return "golemsinthemist";
+                case "_hillsbradcreek_anon": return "hillsbradcreek";
+                case "_losttemple_lv_anon": return "losttemple";
+                case "_marketsquare_anon": return "marketsquare";
+                case "_mur'galoasis_lv_anon": return "murguloasis";
+                case "_nerubianpassage_anon": return "nerubianpassage";
+                case "_northernfelwood_anon": return "northernfelwood";
+                case "_northshire_lv_anon": return "northshire";
+                case "_sanctuary_lv_anon": return "sanctuary";
+                case "_tidewaterglades_lv_anon": return "tidewaterglades";
+                case "_turtlerock_anon": return "turtlerock";
+                case "_twilightruins_lv_anon": return "twilightruins";
+                case "_twilightruins_anon": return "twilightruins";
+
+                default: return mapName;
+            }
         }
 
         public string Name { get; }
