fde64b5070fac807891a63abb77d32e5d151787b
Alexander Beninski <a.beninski@gmail.com>
2020-09-04 15:12:45 +0300

    add tournaments backend (#72)

---
+++ W3ChampionsStatisticService/Tournaments/Models/TournamentMatch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace W3ChampionsStatisticService.Tournaments.Models
+{
+    public class TournamentMatch
+    {
+        public string Id { get; set; }
+        public List<TournamentPlayer> Players { get; set; }
+    }
+}
