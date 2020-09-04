fbcc3d25e610b1ddc6a8871b894265860e508183
Alexander Beninski <a.beninski@gmail.com>
2020-09-04 15:42:55 +0300

    add toxi as admin (#73)

--- W3ChampionsStatisticService/Constants.cs
+++ W3ChampionsStatisticService/Constants.cs
@@ -5,9 +5,13 @@ namespace W3ChampionsStatisticService
     public static class Constants
     {
         public static List<string> ApprovedAdmins =>
-            new List<string>{"gab#1546",
+            new List<string>
+            {
+                "gab#1546",
                 "floss2xdaily#1987",
                 "setcho#2271",
-                "modmoto#2809"};
+                "modmoto#2809",
+                "Toxi#21818"
+            };
     }
 }
