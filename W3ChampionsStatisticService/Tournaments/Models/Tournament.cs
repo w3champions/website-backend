14419917b6dabac342be6cca1d66565311a5b8ca
Alexander Beninski <abeninskibede>
2020-09-04 15:10:17 +0300

    add tournaments backend

---
+++ W3ChampionsStatisticService/Tournaments/Models/Tournament.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+
+namespace W3ChampionsStatisticService.Tournaments.Models
+{
+    public class Tournament
+    {
+        [JsonIgnore]
+        public ObjectId Id { get; set; }
+
+        [JsonPropertyName("id")]
+        public string ObjectId => Id.ToString();
+
+        public string Name { get; set; }
+
+        public List<TournamentRound> WinnerBracketRounds { get; set; }
+        public List<TournamentRound> LoserBracketRounds { get; set; }
+    }
+}
---
+++ W3ChampionsStatisticService/Tournaments/TournamentsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using W3ChampionsStatisticService.ReadModelBase;
+using W3ChampionsStatisticService.Tournaments.Models;
+
+namespace W3ChampionsStatisticService.Tournaments
+{
+    public class TournamentsRepository : MongoDbRepositoryBase
+    {
+        public TournamentsRepository(MongoClient mongoClient) : base(mongoClient)
+        {
+        }
+
+        public async Task<List<Tournament>> GetAll()
+        {
+            var result = await LoadAll<Tournament>();
+
+            return result;
+        }
+
+        public Task Save(Tournament tournament)
+        {
+           return Upsert(tournament, p => p.Id == tournament.Id);
+        }
+    }
+}
