99ca36a0896658534ecd7bed7a13449781d1ee83
Simon Heiss <simonheiss87@gmail.com>
2020-09-10 10:13:44 +0200

    fixed askelange

--- W3ChampionsStatisticService/Admin/Admins.cs
+++ W3ChampionsStatisticService/Admin/Admins.cs
@@ -4,13 +4,14 @@ namespace W3ChampionsStatisticService.Admin
 {
     public static class Admins
     {
+        // battletags have to be lowercased!
         public static List<string> ApprovedAdmins =>
             new List<string>
             {
                 "gab#1546",
                 "floss2xdaily#1987",
                 "setcho#2271",
-                "AskeLange#2705",
+                "askelange#2705",
                 "toxi#21818",
                 "modmoto#2809"
             };
