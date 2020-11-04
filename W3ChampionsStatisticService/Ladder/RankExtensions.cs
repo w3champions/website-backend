e547951d41ebd1a27ee1e7ebd80b8e9264072475
knumf <larsrobertericsson@gmail.com>
2020-11-04 08:51:02 +0100

    search profile if no ranking is found (#91)

---
+++ W3ChampionsStatisticService/Ladder/RankExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using W3ChampionsStatisticService.CommonValueObjects;
+using W3ChampionsStatisticService.PlayerProfiles;
+
+namespace W3ChampionsStatisticService.Ladder
+{
+    public static class RankExtensions
+    {
+        public static Rank CreateUnrankedResponse(this PlayerOverallStats stats)
+        {
+            return new Rank(
+                new List<string> { stats.BattleTag },
+                0,
+                0,
+                0,
+                null,
+                GateWay.Undefined,
+                GameMode.Undefined,
+                0
+                )
+            {
+                Players = new List<PlayerOverview>
+                {
+                    PlayerOverview.Create(new List<PlayerId>
+                    {
+                        new PlayerId
+                        {
+                            BattleTag = stats.BattleTag,
+                            Name = stats.Name
+                        }
+                    }, GateWay.Undefined, GameMode.Undefined, 0, null)
+                }
+            };
+        }
+    }
+}
