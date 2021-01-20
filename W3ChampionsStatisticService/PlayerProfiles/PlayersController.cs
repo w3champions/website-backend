f3ac31059e1a037c58ed95d5147f45547e9a283f
John Graves <johntaylorgraves@gmail.com>
2021-01-20 08:30:23 +0000

    akas WIP

--- W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
+++ W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
@@ -54,6 +54,7 @@ namespace W3ChampionsStatisticService.PlayerProfiles
                 player = PlayerOverallStats.Create(battleTag);
             }
             
+            // Akas are stored in cache - preferences for showing akas are stored in DB
             var settings = await _personalSettingsRepository.Load(battleTag);
             player.PlayerAkaData = _playerAkaProvider.GetAkaDataByPreferences(battleTag, settings);
 
