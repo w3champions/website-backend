c615c621c2ad529c8210aa35451a07efc8d479bd
Alexander Beninski <abeninskibede>
2022-05-27 21:45:51 +0300

    add matchmaking contracts

---
+++ W3ChampionsStatisticService/PadEvents/MatchmakingContracts/MapContract.cs
@@ -0,0 +1,11 @@
+﻿namespace W3ChampionsStatisticService.PadEvents.MatchmakingContracts
+{
+    public class MapContract
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int MaxTeams { get; set; }
+        public MapForce[] MappedForces { get; set; } = new MapForce[0];
+    }
+}
---
+++ W3ChampionsStatisticService/PadEvents/MatchmakingContracts/MapForce.cs
@@ -0,0 +1,9 @@
+﻿namespace W3ChampionsStatisticService.PadEvents.MatchmakingContracts
+{
+    public class MapForce
+    {
+        public int Team { get; set; }
+        public int[] Slots { get; set; }
+        public MapForceComputer[] Computers { get; set; } = new MapForceComputer[0];
+    }
+}
