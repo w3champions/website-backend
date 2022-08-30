fc0cd89c926cb34586a6f3ec8416a020a8a5199d
ShaDeFaDe <carnesen@gmail.com>
2022-08-30 12:55:46 +0200

    Removing comments
    Rebase

--- W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
+++ W3ChampionsStatisticService/PlayerProfiles/PlayersController.cs
@@ -109,10 +109,7 @@ namespace W3ChampionsStatisticService.PlayerProfiles
             GateWay gateWay,
             int season)
         {
-            // var wins = await _queryHandler.LoadPlayerStatsWithRanks(battleTag, gateWay, season);
-
-            // !!! Temp fix until index is added
-            var wins = new List<PlayerGameModeStatPerGateway>();
+            var wins = await _queryHandler.LoadPlayerStatsWithRanks(battleTag, gateWay, season);
             return Ok(wins);
         }
 
