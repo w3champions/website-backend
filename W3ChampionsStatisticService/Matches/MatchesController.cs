a64cd14d0972cc215afbd1d959ab06ed425f7159
ShaDeFaDe <carnesen@gmail.com>
2022-08-30 13:59:39 +0200

    Added caching for count of matchups

--- W3ChampionsStatisticService/Matches/MatchesController.cs
+++ W3ChampionsStatisticService/Matches/MatchesController.cs
@@ -19,7 +19,7 @@ namespace W3ChampionsStatisticService.Matches
             _matchRepository = matchRepository;
             _matchQueryHandler = matchQueryHandler;
         }
-
+         
         [HttpGet("")]
         public async Task<IActionResult> GetMatches(
             int offset = 0,
