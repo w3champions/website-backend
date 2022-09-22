6f54593383e23af71f9fe4e11bf2606e2a35dd62
John Graves <40397054+Cepheid-UK@users.noreply.github.com>
2022-09-22 21:57:31 +0100

    [W3C-168] add replay download (#205)
    * [W3C-168] add replay download
    
    * replay api working state

---
+++ W3ChampionsStatisticService/Replays/ReplaysController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using W3C.Domain.UpdateService;
+
+namespace W3ChampionsStatisticService.Maps
+{
+    [ApiController]
+    [Route("api/replays")]
+    public class ReplaysController : ControllerBase
+    {
+        ReplayServiceClient _replayServiceClient;
+        public ReplaysController(ReplayServiceClient replayServiceClient)
+        {
+            _replayServiceClient = replayServiceClient;
+        }
+
+        [HttpGet("{gameId}")]
+        public async Task<IActionResult> GetReplay(int gameId)
+        {
+            var replayStream = await _replayServiceClient.GenerateReplay(gameId);
+            return File(replayStream, "application/octet-stream", $"{gameId}.w3g");
+        }
+    }
+}
