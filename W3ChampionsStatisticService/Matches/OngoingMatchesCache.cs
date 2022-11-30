e807d665d38e04d366b434d9d178ac40433c1eae
Gustav Andersson <gustav87and@gmail.com>
2022-11-30 19:23:39 +0100

    Only sort by mmr if explicitly set by user

--- W3ChampionsStatisticService/Matches/OngoingMatchesCache.cs
+++ W3ChampionsStatisticService/Matches/OngoingMatchesCache.cs
@@ -24,9 +24,8 @@ namespace W3ChampionsStatisticService.Matches
             return _values.Count(m => (gameMode == GameMode.Undefined || m.GameMode == gameMode)
                                       && (gateWay == GateWay.Undefined || m.GateWay == gateWay)
                                       && (map == "Overall" || m.Map == map)
-                                      && !m.Teams.Any(team => team.Players.Any(player => player.OldMmr < minMmr))
-                                      && !m.Teams.Any(team => team.Players.Any(player => player.OldMmr > maxMmr)));
-
+                                      && (minMmr == 0 || !m.Teams.Any(team => team.Players.Any(player => player.OldMmr < minMmr)))
+                                      && (maxMmr == 3000 || !m.Teams.Any(team => team.Players.Any(player => player.OldMmr > maxMmr))));
         }
 
         public async Task<List<OnGoingMatchup>> LoadOnGoingMatches(
@@ -45,8 +44,8 @@ namespace W3ChampionsStatisticService.Matches
                 .Where(m => (gameMode == GameMode.Undefined || m.GameMode == gameMode)
                             && (gateWay == GateWay.Undefined || m.GateWay == gateWay)
                             && (map == "Overall" || m.Map == map)
-                            && !m.Teams.Any(team => team.Players.Any(player => player.OldMmr < minMmr))
-                            && !m.Teams.Any(team => team.Players.Any(player => player.OldMmr > maxMmr)));
+                            && (minMmr == 0 || !m.Teams.Any(team => team.Players.Any(player => player.OldMmr < minMmr)))
+                            && (maxMmr == 3000 || !m.Teams.Any(team => team.Players.Any(player => player.OldMmr > maxMmr))));
 
             if (sort == "mmrDescending") {
                 matches = matches.OrderByDescending(m => getMaxMmrInMatch(m));
