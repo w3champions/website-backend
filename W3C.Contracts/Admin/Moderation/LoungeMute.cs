91cb44e7ac61619c45301bc13442fa9fb867e86a
Gustav Andersson <gustav87and@gmail.com>
2023-03-26 00:29:48 +0100

    Create lounge mutes (#237)
    * Create lounge mutes
    
    * Fix error codes. Do not send btag in uri to matchmaking. Rename LoungeMuteReadmodel
    
    * Use new moderation endpoint in matchmaking
    
    * Break out LoungeMute to new ModerationController

---
+++ W3C.Contracts/Admin/Moderation/LoungeMute.cs
@@ -0,0 +1,14 @@
+namespace W3C.Contracts.Admin.Moderation
+{
+    public class LoungeMute
+    {
+        public string battleTag { get; set; }
+        public string endDate { get; set; }
+        public string author { get; set;}
+    }
+
+    public class LoungeMuteResponse : LoungeMute
+    {
+        public string insertDate { get; set; }
+    }
+}
