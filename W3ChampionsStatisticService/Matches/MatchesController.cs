44bdf11177383f1f5416f64e76206b8a290ec676
Gustav Andersson <gustav87and@gmail.com>
2023-04-28 16:48:27 +0200

    Change finished matches from S14 to S15

--- W3ChampionsStatisticService/Matches/MatchesController.cs
+++ W3ChampionsStatisticService/Matches/MatchesController.cs
@@ -28,8 +28,8 @@ namespace W3ChampionsStatisticService.Matches
             GameMode gameMode = GameMode.Undefined)
         {
             if (pageSize > 100) pageSize = 100;
-            var matches = await _matchRepository.Load(14, gameMode, offset, pageSize);
-            var count = await _matchRepository.Count(14, gameMode);
+            var matches = await _matchRepository.Load(15, gameMode, offset, pageSize);
+            var count = await _matchRepository.Count(15, gameMode);
             return Ok(new { matches, count });
         }
 
