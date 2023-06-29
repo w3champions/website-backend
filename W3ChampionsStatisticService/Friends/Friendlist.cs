856529ab07f02d8f6a637e1acf5b397733b891d2
Gustav Andersson <gustav87and@gmail.com>
2023-06-29 15:34:22 +0200

    Make friendlist, first iteration (#252)

---
+++ W3ChampionsStatisticService/Friends/Friendlist.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using W3C.Contracts.GameObjects;
+using W3C.Domain.CommonValueObjects;
+using W3C.Domain.Repositories;
+
+namespace W3ChampionsStatisticService.Friends
+{
+    public class Friendlist : IIdentifiable
+    {
+        public Friendlist(string battleTag)
+        {
+            Id = battleTag;
+            Friends = new List<string> {};
+            ReceivedFriendRequests = new List<string> {};
+            BlockedBattleTags = new List<string> {};
+            BlockAllRequests = false;
+        }
+
+        public string Id { get; set; }
+        public List<string> Friends { get; set; }
+        public List<string> ReceivedFriendRequests  { get; set; }
+        public List<string> BlockedBattleTags  { get; set; }
+        public bool BlockAllRequests  { get; set; }
+    }
+}
