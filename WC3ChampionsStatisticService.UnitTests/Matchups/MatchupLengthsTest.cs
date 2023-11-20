f57d1ecd6a5f80f48b4b3480ed2572e94df30a87
Kledilson Ferreira <kledilson.ferreira@gmail.com>
2023-11-20 15:28:31 -0300

    remove dead code

--- WC3ChampionsStatisticService.UnitTests/Matchups/MatchupLengthsTest.cs
+++ WC3ChampionsStatisticService.UnitTests/Matchups/MatchupLengthsTest.cs
@@ -55,46 +55,5 @@ public class MatchupLengthsTest : IntegrationTestBase
         Assert.AreEqual(1, matchupLengthForAllSeasons.LengthsByMmrRange["all"][7].Games);
         Assert.AreEqual(180, matchupLengthForAllSeasons.LengthsByMmrRange["all"][6].Seconds);
         Assert.AreEqual(210, matchupLengthForAllSeasons.LengthsByMmrRange["all"][7].Seconds);
-        
-
-        // var sumOpponentRaceLengths =
-        //     gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.NE.ToString("D")].Count 
-        //     + gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.UD.ToString("D")].Count;
-
-        // // check first element
-        // Assert.AreEqual(5, gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.Total.ToString("D")][0]);
-        
-        // // check count of lengths
-        // Assert.AreEqual(4, gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.Total.ToString("D")].Count);
-        // Assert.AreEqual(sumOpponentRaceLengths, gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.Total.ToString("D")].Count);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.NE.ToString("D")].Count);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic1.GameLengthsByOpponentRace[Race.UD.ToString("D")].Count);
-        
-        // // check avg
-        // Assert.AreEqual(10, gameLengthForPlayerStatistic1.AverageGameLengthByOpponentRace[Race.Total.ToString("D")]);
-        // Assert.AreEqual(10, gameLengthForPlayerStatistic1.AverageGameLengthByOpponentRace[Race.NE.ToString("D")]);
-        // Assert.AreEqual(10, gameLengthForPlayerStatistic1.AverageGameLengthByOpponentRace[Race.UD.ToString("D")]);
-        
-        // // should not have key that did not play against
-        // Assert.False(gameLengthForPlayerStatistic1.AverageGameLengthByOpponentRace.ContainsKey(Race.HU.ToString("D")));
-        // Assert.False(gameLengthForPlayerStatistic1.GameLengthsByOpponentRace.ContainsKey(Race.HU.ToString("D")));
-        
-        // // check intervals
-        // Assert.AreEqual(4, gameLengthForPlayerStatistic1.PlayerGameLengthIntervalByOpponentRace[Race.Total.ToString("D")].Lengths["0"]);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic1.PlayerGameLengthIntervalByOpponentRace[Race.NE.ToString("D")].Lengths["0"]);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic1.PlayerGameLengthIntervalByOpponentRace[Race.UD.ToString("D")].Lengths["0"]);
-
-        // // should not have key that did not play against
-        // Assert.False(gameLengthForPlayerStatistic1.PlayerGameLengthIntervalByOpponentRace.ContainsKey(Race.HU.ToString("D")));
-
-        // // assure generated data for opponent as well
-        // var gameLengthForPlayerStatistic2 = await playerRepo.LoadGameLengthForPlayerStats("crazy#2", 5);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic2.GameLengthsByOpponentRace[Race.Total.ToString("D")].Count);
-        // Assert.AreEqual(15, gameLengthForPlayerStatistic2.GameLengthsByOpponentRace[Race.Total.ToString("D")][0]);
-        // Assert.AreEqual(15, gameLengthForPlayerStatistic2.AverageGameLengthByOpponentRace[Race.Total.ToString("D")]);
-        // Assert.AreEqual(2, gameLengthForPlayerStatistic2.GameLengthsByOpponentRace[Race.HU.ToString("D")].Count);
-        // Assert.AreEqual(15, gameLengthForPlayerStatistic2.AverageGameLengthByOpponentRace[Race.HU.ToString("D")]);
-        // Assert.False(gameLengthForPlayerStatistic2.AverageGameLengthByOpponentRace.ContainsKey(Race.UD.ToString("D")));
-        // Assert.False(gameLengthForPlayerStatistic2.GameLengthsByOpponentRace.ContainsKey(Race.NE.ToString("D")));
     }
 }
