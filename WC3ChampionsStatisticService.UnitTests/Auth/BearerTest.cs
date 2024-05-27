4e54e88bc8e44b08fdc2bb8bfa792a5564335a8a
Konstantin <39737614+healfy@users.noreply.github.com>
2024-05-27 18:02:04 +0300

    Change to classic bearer auth instead of using query params (#303)

---
+++ WC3ChampionsStatisticService.UnitTests/Auth/BearerTest.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Primitives;
+using Microsoft.IdentityModel.Tokens;
+using NUnit.Framework;
+using W3ChampionsStatisticService.WebApi.ActionFilters;
+
+namespace WC3ChampionsStatisticService.Tests.Auth;
+
+[TestFixture]
+public class BearerTest
+{
+
+    [Test]
+    public void GetTokenFailed()
+    {
+        Assert.Throws<SecurityTokenValidationException>(() => BearerHasPermissionFilter.GetToken(StringValues.Empty));
+    }
+    
+    [Test]
+    public void GetTokenSuccess()
+    {
+        
+        var result = BearerHasPermissionFilter.GetToken(new StringValues("Bearer fake_token_value"));
+        Assert.AreEqual("fake_token_value", result);
+    }
+}
