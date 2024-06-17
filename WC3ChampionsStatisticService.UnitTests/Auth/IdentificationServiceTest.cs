847ae42f5999fcd7dba96053c5ceb8ab6fafc9a3
konstantin.zavadsky <healfy92@gmail.com>
2024-06-17 20:27:17 +0200

    small refactor

--- WC3ChampionsStatisticService.UnitTests/Auth/IdentificationServiceTest.cs
+++ WC3ChampionsStatisticService.UnitTests/Auth/IdentificationServiceTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using Moq;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using W3C.Domain.IdentificationService;
 
 namespace WC3ChampionsStatisticService.Tests.Auth;
@@ -9,10 +10,14 @@ namespace WC3ChampionsStatisticService.Tests.Auth;
 [TestFixture]
 public class IdentificationServiceTest
 {
+    [SetUp]
+    public void Setup()
+    {
+        Environment.SetEnvironmentVariable("WEBSITE_BACKEND_TO_ID_SERVICE_SECRET", "e72dbdaa8b0d89d8fa5eaa2620f31e75186081a555ea14df9202ad6a9f180653");
+    }
     [Test]
     public void TestTokenCache()
     {
-        Environment.SetEnvironmentVariable("WEBSITE_BACKEND_TO_ID_SERVICE_SECRET", "e72dbdaa8b0d89d8fa5eaa2620f31e75186081a555ea14df9202ad6a9f180653");
         var mockFactory = new Mock<IHttpClientFactory>();
         mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(new HttpClient());
         var client = new IdentificationServiceClient(mockFactory.Object);
@@ -23,7 +28,6 @@ public class IdentificationServiceTest
     [Test]
     public void TestTokenNew()
     {
-        Environment.SetEnvironmentVariable("WEBSITE_BACKEND_TO_ID_SERVICE_SECRET", "e72dbdaa8b0d89d8fa5eaa2620f31e75186081a555ea14df9202ad6a9f180653");
         var mockFactory = new Mock<IHttpClientFactory>();
         mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(new HttpClient());
         var client = new IdentificationServiceClient(mockFactory.Object, 0.001);
