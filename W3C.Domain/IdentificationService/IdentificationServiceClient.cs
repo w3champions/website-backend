7f49f14b8d4d0eef11a23299c4e84aa5182e9f4d
konstantin.zavadsky <healfy92@gmail.com>
2024-06-17 20:21:20 +0200

    add tests

--- W3C.Domain/IdentificationService/IdentificationServiceClient.cs
+++ W3C.Domain/IdentificationService/IdentificationServiceClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
@@ -14,14 +15,18 @@ namespace W3C.Domain.IdentificationService;
 public class IdentificationServiceClient
 {
     private readonly HttpClient _httpClient;
-    private readonly string _identificationSecret = Environment.GetEnvironmentVariable("WEBSITE_BACKEND_TO_ID_SERVICE_SECRET");
+    private readonly string _identificationSecret = Environment.GetEnvironmentVariable("WEBSITE_BACKEND_TO_ID_SERVICE_SECRET"); 
+    // should be at least 32 bytes  'e72dbdaa8b0d89d8fa5eaa2620f31e75186081a555ea14df9202ad6a9f180653'
     private static readonly string ServiceApiUrl = Environment.GetEnvironmentVariable("IDENTIFICATION_SERVICE_URI") ?? "http://localhost:8081";
     private string _cachedToken;
-    private DateTime _tokenExpiryTime;
+    private readonly double _expireTime;
+    private DateTime? _tokenExpiryTime;
+    
 
-    public IdentificationServiceClient(IHttpClientFactory httpClientFactory)
+    public IdentificationServiceClient(IHttpClientFactory httpClientFactory, double expireTimeSeconds = 600)
     {
         _httpClient = httpClientFactory.CreateClient();
+        _expireTime = expireTimeSeconds;
     }
     public async Task<bool> HasPermission(EPermission ePermission, string battleTag)
     {
@@ -38,24 +43,24 @@ public class IdentificationServiceClient
     {
         var secret = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_identificationSecret));
         var tokenHandler = new JwtSecurityTokenHandler();
-        _tokenExpiryTime = DateTime.UtcNow.AddMinutes(10);
+        _tokenExpiryTime = DateTime.UtcNow.AddSeconds(_expireTime);
         var jwt = new JwtSecurityToken(
             issuer: "w3c-website-backend",
             audience: "w3c-identification-service",
             signingCredentials: new SigningCredentials(secret, SecurityAlgorithms.HmacSha256Signature),
-            expires: _tokenExpiryTime
+            expires: _tokenExpiryTime,
+            claims: new Claim[]
+            {
+                new("uniq",  Guid.NewGuid().ToString()),
+            }
+            
         );
         return tokenHandler.WriteToken(jwt);
-        ;
     }
 
     private async Task<string> MakePostRequest(string url, object requestBody)
     {
-        if (string.IsNullOrEmpty(_cachedToken) || DateTime.UtcNow >= _tokenExpiryTime)
-        {
-            _cachedToken = GenerateToken();
-        }
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _cachedToken);
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GetToken());
         var result = await _httpClient.PostAsync(
             url,
             new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json")
@@ -66,5 +71,14 @@ public class IdentificationServiceClient
             return response;
         }
         throw new HttpRequestException($"Invalid request from IdentificationService code={result.StatusCode}, response={response}");
+    }
+
+    public string GetToken()
+    {
+        if (string.IsNullOrEmpty(_cachedToken) || DateTime.UtcNow >= _tokenExpiryTime)
+        {
+            _cachedToken = GenerateToken();
+        }
+        return _cachedToken;
     }
 }
