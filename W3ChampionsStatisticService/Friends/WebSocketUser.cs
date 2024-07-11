eff4aaa0405bad5fb1f3e0eb324d7cf026cc4e2e
Gustav Andersson <gustav87and@gmail.com>
2024-07-11 12:00:35 +0200

    move WebSocketUser from Friends namespace to Hubs namespace

--- W3ChampionsStatisticService/Friends/WebSocketUser.cs
+++
@@ -1,7 +0,0 @@
-namespace W3ChampionsStatisticService.Friends;
-
-public class WebSocketUser
-{
-    public string BattleTag { get; set; }
-    public string ConnectionId {get; set; }
-}
--- W3ChampionsStatisticService/Hubs/ConnectionMapping.cs
+++ W3ChampionsStatisticService/Hubs/ConnectionMapping.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using W3ChampionsStatisticService.Friends;
 
 namespace W3ChampionsStatisticService.Hubs;
 
