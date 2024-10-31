22ae6c74b0320210bbefbea001cac521eebbf0d8
gremlinhunter <186614177+gremlinhunter@users.noreply.github.com>
2024-10-31 18:58:23 +0100

    redo tests

--- WC3ChampionsStatisticService.UnitTests/Statistics/GameLengthStatTests.cs
+++ WC3ChampionsStatisticService.UnitTests/Statistics/GameLengthStatTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using NUnit.Framework;
 using W3C.Contracts.Matchmaking;
 using W3ChampionsStatisticService.W3ChampionsStats.GameLengths;
@@ -12,19 +13,24 @@ public class GameLengthStatTests : IntegrationTestBase
     public void GameLengthStatsBelow30s()
     {
         var gameLengthStats = GameLengthStat.Create(GameMode.GM_1v1);
-        gameLengthStats.Apply(GameMode.GM_1v1, new TimeSpan(0, 0, 20));
+        var mmr = 1500;
+        gameLengthStats.Apply(20, mmr, GameMode.GM_1v1);
 
-        Assert.AreEqual(1, gameLengthStats.Lengths[0].Games);
-        Assert.AreEqual(0, gameLengthStats.Lengths[1].Games);
+        Assert.AreEqual(1, gameLengthStats.LengthsByMmrRange["all"][0].Games);
+        Assert.AreEqual(0, gameLengthStats.LengthsByMmrRange["all"][1].Games);
     }
 
     [Test]
     public void GameLengthStatsLongerThan1hour()
     {
         var gameLengthStats = GameLengthStat.Create(GameMode.GM_1v1);
-        gameLengthStats.Apply(GameMode.GM_1v1, new TimeSpan(1, 5, 20));
+        var duration = (int)new TimeSpan(1, 5, 20).TotalSeconds;
+        var mmr = 1500;
+        gameLengthStats.Apply(duration, mmr, GameMode.GM_1v1);
 
-        Assert.AreEqual(1, gameLengthStats.Lengths[120].Games);
-        Assert.AreEqual(3600, gameLengthStats.Lengths[120].Seconds);
+        var playedGame = gameLengthStats.LengthsByMmrRange["all"].FindIndex(gl => gl.Games > 0);
+        
+        Assert.AreEqual(1, gameLengthStats.LengthsByMmrRange["all"][playedGame].Games);
+        Assert.AreEqual(3600, gameLengthStats.LengthsByMmrRange["all"][playedGame].Seconds);
     }
 }
