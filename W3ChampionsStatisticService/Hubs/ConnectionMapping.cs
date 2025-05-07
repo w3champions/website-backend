c9eb1762184869e8a4e3db81567836f38953a052
Magenta <11004447+Magentah@users.noreply.github.com>
2025-05-07 17:50:59 +0900

    Update websockets to include friend online status

--- W3ChampionsStatisticService/Hubs/ConnectionMapping.cs
+++ W3ChampionsStatisticService/Hubs/ConnectionMapping.cs
@@ -6,10 +6,12 @@ namespace W3ChampionsStatisticService.Hubs;
 public class ConnectionMapping
 {
     private readonly Dictionary<string, WebSocketUser> _connections = new();
+    private readonly Dictionary<string, HashSet<string>> _onlineUsersByBattleTag = new();
+    private readonly object _lock = new();
 
     public List<WebSocketUser> GetUsers()
     {
-        lock (_connections)
+        lock (_lock)
         {
             return _connections.Values.Select(v => v).OrderBy(r => r.BattleTag).ToList();
         }
@@ -17,29 +19,54 @@ public class ConnectionMapping
 
     public void Add(string connectionId, WebSocketUser user)
     {
-        lock (_connections)
+        lock (_lock)
         {
             if (!_connections.ContainsKey(connectionId))
             {
                 _connections.Add(connectionId, user);
+                if (!_onlineUsersByBattleTag.ContainsKey(user.BattleTag))
+                {
+                    _onlineUsersByBattleTag[user.BattleTag] = new HashSet<string>();
+                }
+                _onlineUsersByBattleTag[user.BattleTag].Add(connectionId);
             }
         }
     }
 
     public WebSocketUser GetUser(string connectionId)
     {
-        lock (_connections)
+        lock (_lock)
         {
             _connections.TryGetValue(connectionId, out WebSocketUser user);
             return user;
         }
     }
 
+    public bool IsUserOnline(string battleTag)
+    {
+        lock (_lock)
+        {
+            return _onlineUsersByBattleTag.ContainsKey(battleTag) && _onlineUsersByBattleTag[battleTag].Count > 0;
+        }
+    }
+
     public void Remove(string connectionId)
     {
-        lock (_connections)
+        lock (_lock)
         {
-            _connections.Remove(connectionId);
+            if (_connections.TryGetValue(connectionId, out var user))
+            {
+                _connections.Remove(connectionId);
+
+                if (_onlineUsersByBattleTag.TryGetValue(user.BattleTag, out var userConnections))
+                {
+                    userConnections.Remove(connectionId);
+                    if (userConnections.Count == 0)
+                    {
+                        _onlineUsersByBattleTag.Remove(user.BattleTag);
+                    }
+                }
+            }
         }
     }
 }
