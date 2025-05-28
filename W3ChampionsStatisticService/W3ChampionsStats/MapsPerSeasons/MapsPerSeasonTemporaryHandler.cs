4d9e83e0decaf58fa627428b362b2e5a05037a8a
Coff <82292207+CoffMakesThings@users.noreply.github.com>
2025-05-28 18:35:25 +0300

    Update MapsPerSeasonTemporaryHandler.cs

--- W3ChampionsStatisticService/W3ChampionsStats/MapsPerSeasons/MapsPerSeasonTemporaryHandler.cs
+++ W3ChampionsStatisticService/W3ChampionsStats/MapsPerSeasons/MapsPerSeasonTemporaryHandler.cs
@@ -9,7 +9,7 @@ namespace W3ChampionsStatisticService.W3ChampionsStats.MapsPerSeasonTemporaryHan
 
 // This handler will add mapNames to previously stored GamesPlayedOnMap objects in the MapsPerSeason collection
 // This handler could be removed after going through currently stored MatchFinishedEvents
-public class MapsPerSeasonTemporaryHandler(IW3StatsRepo w3Stats) : IReadModelHandler
+public class MapsPerSeasonTemporaryHandler(IW3StatsRepo w3Stats) : IMatchFinishedReadModelHandler
 {
     private readonly IW3StatsRepo _w3Stats = w3Stats;
 
