75eb5286a04a9093d7212f6185c913ba7e3342ee
Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>
2025-06-04 06:22:30 +0200

    Improve permission filters

--- W3ChampionsStatisticService/WebApi/ActionFilters/InjectActingPlayerAuthCodeAttribute.cs
+++ W3ChampionsStatisticService/WebApi/ActionFilters/InjectActingPlayerAuthCodeAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,5 +13,14 @@ public class InjectActingPlayerAuthCodeAttribute : Attribute, IFilterFactory
     public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
     {
         return serviceProvider.GetService<InjectActingPlayerFromAuthCodeFilter>();
+    }
+
+    public static W3CUserAuthenticationDto GetActingPlayerUser(HttpContext context)
+    {
+        if (context.Items.TryGetValue(InjectActingPlayerFromAuthCodeFilter.ActingPlayerUserKey, out var value))
+        {
+            return value as W3CUserAuthenticationDto;
+        }
+        throw new Exception("No acting player user found in context");
     }
 }
