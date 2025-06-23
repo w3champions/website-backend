66d67221519fb5f224b74ae92cc6a4783bca8a04
Marco de Abreu <marcoabreu@users.noreply.github.com>
2025-06-24 00:46:09 +0200

    Skip processing match for mmr rp timeline if the player was in AT (#437)
    * Skip processing match for mmr rp timeline if the player was in an arranged team
    
    * Add tests
    
    * Dotnet format
    
    ---------
    
    Co-authored-by: Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>

--- W3ChampionsStatisticService/PlayerProfiles/MmrRankingStats/PlayerMmrRpTimelineHandler.cs
+++ W3ChampionsStatisticService/PlayerProfiles/MmrRankingStats/PlayerMmrRpTimelineHandler.cs
@@ -4,6 +4,7 @@ using W3C.Domain.MatchmakingService;
 using W3ChampionsStatisticService.Ports;
 using W3ChampionsStatisticService.ReadModelBase;
 using W3C.Domain.Tracing;
+using Serilog;
 
 namespace W3ChampionsStatisticService.PlayerProfiles.MmrRankingStats;
 
@@ -16,9 +17,25 @@ public class PlayerMmrRpTimelineHandler(IPlayerRepository playerRepository) : IM
     {
         var match = nextEvent.match;
 
+
+        if (match.endTime == 0)
+        {
+            Log.Information("Finished match {FinishedMatchId} has no end time, skipping processing", match.id);
+            return;
+        }
+
         foreach (var player in match.players)
         {
-            if (player.updatedMmr == null || match.endTime == 0) { return; }
+            if (player.IsAt)
+            {
+                Log.Information("Player {Player} in finished match {FinishedMatchId} is in an arranged team {AtTeamId}, skipping processing", player.battleTag, match.id, player.atTeamId);
+                continue;
+            }
+            if (player.updatedMmr == null)
+            {
+                Log.Information("Player {Player} in finished match {FinishedMatchId} has no updated MMR, skipping processing", player.battleTag);
+                continue;
+            }
             var mmrRpTimeline = await _playerRepository.LoadPlayerMmrRpTimeline(player.battleTag, player.race, match.gateway, match.season, match.gameMode)
                         ?? new PlayerMmrRpTimeline(player.battleTag, player.race, match.gateway, match.season, match.gameMode);
             mmrRpTimeline.UpdateTimeline(new MmrRpAtDate(
