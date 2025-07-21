e1851b9b36066f0443ea3ea2c3b6a6a9d082c471
Marco de Abreu <marcoabreu@users.noreply.github.com>
2025-07-21 13:30:08 +0200

    Add shadow ban and reason (#449)
    Co-authored-by: Marco de Abreu <18629099+marcoabreu@users.noreply.github.com>

--- W3C.Contracts/Admin/Moderation/LoungeMute.cs
+++ W3C.Contracts/Admin/Moderation/LoungeMute.cs
@@ -5,6 +5,8 @@ public class LoungeMute
     public string battleTag { get; set; }
     public string endDate { get; set; }
     public string author { get; set; }
+    public string reason { get; set; }
+    public bool isShadowBan { get; set; }
 }
 
 public class LoungeMuteResponse : LoungeMute
